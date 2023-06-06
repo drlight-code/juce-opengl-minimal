259ab1755b941368b1070c77636f196983ae6f66
Patric Schmitz <bzk0711@aol.com>
2023-06-06 13:02:19 +0200

    minimal example to reproduce offscreen rendering issues
    https://forum.juce.com/t/part-of-output-not-visible-when-2d-drawing-with-opengl-context/56547

--- src/GLComponent.cc
+++ src/GLComponent.cc
@@ -1,8 +1,29 @@
 #include "GLComponent.hh"
 
+
+class GLContextGraphics {
+public:
+  GLContextGraphics(juce::OpenGLContext &glContext, int width, int height)
+      : _glRenderer(
+            juce::createOpenGLGraphicsContext(glContext, width, height)),
+        _graphics(*_glRenderer) {
+    _graphics.addTransform(
+        juce::AffineTransform::scale(float(glContext.getRenderingScale())));
+  }
+
+  ~GLContextGraphics() { _glRenderer = nullptr; }
+
+  juce::Graphics &get() { return _graphics; }
+
+private:
+  std::unique_ptr<juce::LowLevelGraphicsContext> _glRenderer;
+  juce::Graphics _graphics;
+};
+
 GLComponent::GLComponent ()
 {
   _glContext.setRenderer (this);
+  _glContext.setComponentPaintingEnabled(false);
   _glContext.setContinuousRepainting (true);
   _glContext.attachTo (*this);
 }
@@ -12,15 +33,16 @@ GLComponent::~GLComponent ()
   _glContext.detach ();
 }
 
-void
-GLComponent::paint (juce::Graphics &g)
-{
-  g.fillAll (juce::Colours::aquamarine);
-}
+// void
+// GLComponent::paint (juce::Graphics &g)
+// {
+// }
 
 void
 GLComponent::resized ()
 {
+  auto lock = std::lock_guard<std::mutex>(_mutexBounds);
+  _bounds = getLocalBounds();
 }
 
 void
@@ -34,15 +56,90 @@ GLComponent::newOpenGLContextCreated ()
 void
 GLComponent::renderOpenGL ()
 {
-  static auto lastT = std::chrono::high_resolution_clock::now ();
+  using namespace juce::gl;
+  using juce::OpenGLHelpers;
 
-  auto now = std::chrono::high_resolution_clock::now ();
-  auto deltaT
-      = std::chrono::duration_cast<std::chrono::microseconds> (now - lastT)
-            .count ();
-  lastT = now;
+  jassert(OpenGLHelpers::isContextActive());
 
-  juce::Logger::writeToLog ("frametime: " + juce::String (deltaT/1000.f));
+  updateBounds();
+
+  GLContextGraphics graphics(_glContext, //
+                             _boundsRender.getWidth(),
+                             _boundsRender.getHeight());
+
+  OpenGLHelpers::clear(juce::Colours::black);
+
+  auto constexpr drawOnScreen = false;
+
+  if(drawOnScreen) {
+    drawBlobs(graphics.get());
+  }
+  else {
+    auto fbo = juce::OpenGLImageType::getFrameBufferFrom(*_imageBlend);
+    fbo->makeCurrentAndClear();
+
+    juce::Graphics gFBO{*_imageBlend};
+    drawBlobs(gFBO);
+
+    fbo->releaseAsRenderingTarget();
+
+    graphics.get().drawImage(*_imageBlend, _boundsRender.toFloat());
+  }
+}
+
+void
+GLComponent::drawBlobs(juce::Graphics & g)
+{
+  using namespace juce::gl;
+
+  auto constexpr blobSize = 500.f;
+
+  auto constexpr drawRect = true;
+  auto constexpr useLayer = false;
+
+  if (useLayer)
+    g.beginTransparencyLayer(1.f);
+
+  auto area = juce::Rectangle<float>(100.f, 100.f, blobSize, blobSize);
+  g.setColour(juce::Colours::red);
+  drawRect ? g.fillRect(area) : g.fillEllipse(area);
+
+  area.translate(blobSize / 2.f, 0);
+  g.setColour(juce::Colours::blue);
+  drawRect ? g.fillRect(area) : g.fillEllipse(area);
+
+  area.translate(blobSize / 2.f, 0);
+  g.setColour(juce::Colours::green);
+  drawRect ? g.fillRect(area) : g.fillEllipse(area);
+
+  area.translate(blobSize / 2.f, 0);
+  g.setColour(juce::Colours::white);
+  drawRect ? g.fillRect(area) : g.fillEllipse(area);
+
+  if (useLayer)
+    g.endTransparencyLayer();
+}
+
+void
+GLComponent::updateBounds()
+{
+  auto lock = std::lock_guard<std::mutex>(_mutexBounds);
+  if (_bounds != _boundsRender) {
+    _boundsRender = _bounds;
+    renderBoundsChanged();
+  }
+}
+
+void
+GLComponent::renderBoundsChanged() {
+  juce::Logger::writeToLog(juce::String("bounds changed: ") +
+                           juce::String(_boundsRender.getWidth()) + " x " +
+                           juce::String(_boundsRender.getHeight()));
+
+  _imageBlend = std::make_unique<juce::Image>(juce::Image::PixelFormat::ARGB, //
+                                              _boundsRender.getWidth(),
+                                              _boundsRender.getHeight(), //
+                                              false, juce::OpenGLImageType());
 }
 
 void
--- src/GLComponent.hh
+++ src/GLComponent.hh
@@ -9,12 +9,25 @@ public:
   ~GLComponent ();
 
   void resized () override;
-  void paint (juce::Graphics &g) override;
+  // void paint (juce::Graphics &g) override;
 
   void newOpenGLContextCreated () override;
   void renderOpenGL () override;
   void openGLContextClosing () override;
 
 private:
-  juce::OpenGLContext _glContext;
+  void updateBounds();
+  void renderBoundsChanged();
+  void drawBlobs(juce::Graphics &g);
+
+      juce::OpenGLContext _glContext;
+  std::unique_ptr<juce::Image> _imageBlend;
+
+  // component bounds updated by the UI thread
+  juce::Rectangle<int> _bounds;
+
+  // copy of the component bounds for asynchronous use in the GL
+  // renderer thread
+  std::mutex _mutexBounds;
+  juce::Rectangle<int> _boundsRender;
 };
